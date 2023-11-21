2a33ed7be72feff6c882e9a2b333db2b542a6771
Vynu2908 <64244062+Vynu2908@users.noreply.github.com>
2023-11-21 20:02:15 +0530

    Add files via upload

---
+++ Components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+
+import './footer.scss'
+const Footer = () => {
+  return (
+    <div className='footer'>
+    <span>Vynu Dash Board</span>
+    <span>Copy right of the Company</span>
+    </div>
+  )
+}
+
+export default Footer
